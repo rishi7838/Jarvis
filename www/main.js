4d6846a061e57de66659af43de827d15d3a65b96
Rishi <146337660+rishi7838@users.noreply.github.com>
2025-04-24 18:10:14 +0530

    2nd_day_work_Done

--- www/main.js
+++ www/main.js
@@ -11,5 +11,33 @@ $(document).ready(function () {
 
         },
         
-    })
+    });
+
+    // siri waves configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+      });
+
+      //siri msg animation
+
+      $('.siri-message').textillate({
+        loop:true,
+        sync:true,
+        in:{
+            effect:"fadeInUp",
+            sync:true,
+
+        },
+         out:{
+            effect:"fadeOutUp",
+            sync:true,
+        },
+        
+    });
 });
